dd362f8cdb09f9654d6cfafc4a0ee8163c68bc46
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-09-16 21:43:02 +0300

    Add files via upload

---
+++ 1_homework/5.py
@@ -0,0 +1,12 @@
+x = input('Vvod x: ')
+y = input('Vvod x: ')
+x = float(x)
+y = float(y)
+if x > 0 and y > 0:
+    print('1 4etvert')
+elif x < 0 and y > 0:
+    print('2 4etvert')
+elif x < 0 and y < 0:
+    print('3 4etvet')
+elif x > 0 and y < 0:
+    print('4 4etvert')
