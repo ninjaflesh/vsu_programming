7626dffa074942d1f1d7018739804ba03c01240d
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-09-27 19:54:29 +0300

    Update 4.py

--- 2_homework/4.py
+++ 2_homework/4.py
@@ -1,14 +1,19 @@
+def skobki(r):
+    ot = 0
+    zk = 0
+    for x in r:
+        if x == "(":
+            ot += 1
+        elif x == ")":
+            zk += 1
+    if ot > zk:
+        return("Не хватает 1 закрывающей скобки!")
+    elif ot < zk:
+        return("Не хватает 1 открывающей скобки!")
+    else:
+        return("Каеф!")
+
+
 lul = input("Vvod: ")
-ot = 0
-zk = 0
-for x in lul:
-    if x == "(":
-        ot += 1
-    elif x == ")":
-        zk += 1
-if ot > zk:
-    print("Не хватает 1 закрывающей скобки!")
-elif ot < zk:
-    print("Не хватает 1 открывающей скобки!")
-else:
-    print("Каеф!")
+otv = skobki(lul)
+print(otv)
