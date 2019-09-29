b14b3fa5185484ea983d628f77ccb7cff16886bc
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-09-29 16:17:06 +0300

    Update 2.py

--- 2_homework/2.py
+++ 2_homework/2.py
@@ -1,10 +1,7 @@
 s = []
 lul = input("Vvod: ")
-otv = ""
 while lul:
     s.append(lul)
     lul = input("Vvod: ")
-s = sorted(s)[::-1]
-for x in s:
-    otv += x
-print(otv)
+s = "".join(sorted(s)[::-1])
+print(s)
