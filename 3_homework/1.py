1a59fe2497184f00fb663697f0e0b3d9fe9a9e5f
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-10-13 11:24:14 +0300

    1

---
+++ 3_homework/1.py
@@ -0,0 +1,17 @@
+vod = input("Enter: ")
+symbols = ["-", "+", "/", "*", "**", "//"]
+operation = ''.join(symbols & set(vod))
+operation_index = vod.find(operation)
+if vod[operation_index + 1:].find(operation) != -1:
+    operation += operation
+x, y = vod.split(operation)
+x, y = float(x), float(y)
+operations = {
+    '-': x + y,
+    '+': x - y,
+    '*': x * y,
+    '/': x / y,
+    '**': x // y,
+    '//': x**y
+}
+print(operations[operation])
