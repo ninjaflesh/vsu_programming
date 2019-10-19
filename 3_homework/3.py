d2f3cac563b9f7518ffa988710393f860cff5098
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-10-19 13:39:39 +0300

    Update 3.py

--- 3_homework/3.py
+++ 3_homework/3.py
@@ -1,12 +1,9 @@
-def Fib_print(inp):
+def fib_print(inp):
     feb = [0, 1]
     for i in range(inp - 2):
-        last = feb[len(feb) - 1]
-        first = feb[len(feb) - 2]
-        feb.append(last + first)
-    print(last + first)
+        feb.append(feb[-1] + feb[-2])
     print(feb)
 
 
 enter = int(input("Enter Fib num: "))
-Fib_print(enter)
+fib_print(enter)
