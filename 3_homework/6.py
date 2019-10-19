86bb1ae226ea51a6656672dde494bc5242a32edd
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-10-19 13:39:52 +0300

    Update 6.py

--- 3_homework/6.py
+++ 3_homework/6.py
@@ -1,7 +1,11 @@
+from math import sqrt
+
+
 def sort_4isla(x):
-    if x == 2 or x == 3 or x == 5 or x == 7:
+    for i in range(2, int(sqrt(x))+1):
+        if not x % i:
+            return False
         return True
-    return bool(x % 7 and x % 5 and x % 3 and x % 2)
 
 
 enter = int(input())
