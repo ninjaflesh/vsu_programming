bd94a48018235904c7305af6b35259d8d3cd432d
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-10-20 13:58:17 +0300

    Update 7.py

--- 3_homework/7.py
+++ 3_homework/7.py
@@ -3,7 +3,6 @@ enter = input("Vvod: ").split()
 m = list(map(len, enter))
 print(enter[m.index(max(m))])
 
-m.clear()
 m = [enter.count(x) for x in set(enter)]
 
 print(list(set(enter))[m.index(max(m))])
