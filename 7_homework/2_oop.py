8cc38cfd4d192cdb307c4057ea98d4ee12ba7ba1
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2020-02-20 18:32:22 +0300

    Update 2_oop.py

--- 7_homework/2_oop.py
+++ 7_homework/2_oop.py
@@ -1,8 +1,8 @@
 class Matrix:
-    def __init__(self):
+    def __init__(self, mat=[]):
         self.string = None
         self.column = None
-        self.mat = []
+        self.mat = mat
 
     def input_mat(self):
         self.string = int(input('Строки: '))
@@ -14,6 +14,17 @@ class Matrix:
         return '\n'.join(' '.join(str(i) for i in x) for x in self.mat)
 
 
-matrix = Matrix()
-matrix.input_mat()
-print(matrix)
+class Matrix2x2(Matrix):
+    def __init__(self, mat):
+        if list(map(len, mat)).count(2) != 2:
+            raise ValueError("Матрица должна быть 2х2")
+        super().__init__(mat)
+
+    def determinant(self):
+        print(self.mat[0][0] * self.mat[1][1] -
+              self.mat[0][1] * self.mat[1][0])
+
+
+test = [[1, 2], [4, 5]]
+det = Matrix2x2(test)
+det.determinant()
